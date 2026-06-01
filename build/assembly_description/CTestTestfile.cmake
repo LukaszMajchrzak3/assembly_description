f938e8c5536b16561d0ca0d4ec0260c51da0e334
LukaszMajchrzak3 <lukasz.majchrzak@student.put.poznan.pl>
2026-06-01 15:21:33 +0200

    First commit

---
+++ build/assembly_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lucasm/assembly_description
+# Build directory: /home/lucasm/assembly_description/build/assembly_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
