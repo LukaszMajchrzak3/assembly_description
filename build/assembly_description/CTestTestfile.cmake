29ddc05e04c1aa3375ad8b5cc892261fee9a85f3
LukaszMajchrzak3 <lukasz.majchrzak@student.put.poznan.pl>
2026-06-01 21:53:18 +0200

    Deleting logs and unnecessary files

--- build/assembly_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucasm/assembly_description
-# Build directory: /home/lucasm/assembly_description/build/assembly_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
